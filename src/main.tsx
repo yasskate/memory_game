267a685f9e027366360477cbbac9a127cc446196
Yasser Batas - yasskate <yassk8@gmail.com>
2023-03-01 11:10:40 -0600

    Implements styled global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import App from './App'
-import './index.css'
+import GlobalStyle from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={{ mode: 'dark' }}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
 )
