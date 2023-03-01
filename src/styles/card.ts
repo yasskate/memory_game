34d2e5317be9d0bbb2f5141aab3dd266e3ca60a6
Yasser Batas - yasskate <yassk8@gmail.com>
2023-03-01 16:31:30 -0600

    Add card styled component

---
+++ src/styles/card.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  // flex: 1;
+  background-color: green;
+  margin: 0.5em;
+  height: 20%;
+  width: 15%;
+`
+
