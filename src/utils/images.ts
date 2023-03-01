8d43a10b360cd805da979aa99f596b754d22991a
Yasser Batas - yasskate <yassk8@gmail.com>
2023-03-01 16:30:59 -0600

    Add images object util

---
+++ src/utils/images.ts
@@ -0,0 +1,22 @@
+import image001 from '../assets/cards/001.png'
+import image002 from '../assets/cards/002.png'
+import image003 from '../assets/cards/003.png'
+import image004 from '../assets/cards/004.png'
+import image005 from '../assets/cards/005.png'
+import image006 from '../assets/cards/006.png'
+import image007 from '../assets/cards/007.png'
+import image008 from '../assets/cards/008.png'
+import image009 from '../assets/cards/009.png'
+
+export const images = {
+  1: image001,
+  2: image002,
+  3: image003,
+  4: image004,
+  5: image005,
+  6: image006,
+  7: image007,
+  8: image008,
+  9: image009
+}
+
