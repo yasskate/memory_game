00fe1e85dcdd6070ed0ba8601b915601e73832c4
Yasser Batas - yasskate <yassk8@gmail.com>
2023-03-01 16:31:44 -0600

    Add board styled component

---
+++ src/styles/board.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const BoardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  background-color: salmon;
+  width: 70%;
+`
