d9dcd0f62aeadd2bd9d584b00fcb25601a999b67
Yasser Batas - yasskate <yassk8@gmail.com>
2023-03-02 23:05:48 -0600

    Update global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,13 +1,16 @@
 import { createGlobalStyle } from "styled-components"
 
- // export default createGlobalStyle`
  const GlobalStyle = createGlobalStyle`
   * {
-      margin: 0;
-      padding: 0;
-      outline:0;
-      box-sizing:border-box;
-      font-family: 'Open Sans', sans-serif; 
+    margin: 0;
+    padding: 0;
+    outline:0;
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    box-sizing:border-box;
+    font-family: 'Open Sans', sans-serif; 
+    background-color: #242424;
   }
 
   #root {
